0001573b91108f305c7a3fb4b7f751b0031e9353
amirfaraji78 <amirhoseinfaraji784@gmail.com>
2022-02-04 18:00:05 +0330

    write contract

--- contracts/RentHotel.sol
+++ contracts/RentHotel.sol
@@ -1,6 +1,33 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.4.22 <0.9.0;
+pragma solidity >=0.7.0 <0.9.0;
 
 contract RentHotel{
+    enum Statuses {
+        Vacant,
+        Occupied
+    }
+    Statuses currentStatus;
+    event Occupy(address _occuoand, uint _value);
+    address payable public owner;
+
+    constructor(){
+        owner = payable (msg.sender);
+        currentStatus = Statuses.Vacant;
+    }
+
+    modifier onlyWhileVacant {
+        require(currentStatus == Statuses.Vacant, "Currently Occupied");
+        _;
+    }
+
+    modifier costs (uint _amount){
+        require(msg.value>= _amount, "Not enough Ether provided");
+        _;
+    }
     
-}
+    receive() external payable onlyWhileVacant costs(2 ether) {
+        currentStatus = Statuses.Occupied;
+        owner.transfer(msg.value);
+        emit Occupy(msg.sender, msg.value);
+    }
+} 
